df6bdb3bdebd66ad280eba555db56b5be7fbe6e4
lcysweet <lcy@163.com>
2019-08-08 18:19:45 +0800

    demo第一次提交

---
+++ base/base_analyze.py
@@ -0,0 +1,17 @@
+import yaml
+
+
+def analyze_file(file_name, key_name):
+    # contact_data
+    file_path = "./data/" + file_name + ".yaml"
+    with open(file_path, "r", encoding='utf-8') as f:
+
+        data = yaml.load(f)
+        data_list = list()
+        for i in data[key_name].values():
+            data_list.append(list(i.values()))
+        # return data_list
+        print(data_list);
+
+
+analyze_file("contact_data", "test_add_contact");
---
+++ scripts/test_login.py
@@ -0,0 +1,7 @@
+class TestLogin:
+
+    def test_login01(self):
+        assert 1
+
+    def test_login02(self):
+        assert 0
