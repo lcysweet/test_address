fdffbc1fb1ba0618c24422cc11f7f1905af867f3
lcysweet <lcy@163.com>
2019-08-09 13:00:46 +0800

    编写参数化脚本配置执行用例

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,8 +1,12 @@
 import time
+import pytest
 import sys
 sys.path.append('D:\\Projects\workes\\user_login')
 from base.base_driver import init_driver
 from page.page import Page
+from base.base_analyze import analyze_file
+
+
 class TestLogin:
 
     def setup(self):
@@ -12,14 +16,14 @@ class TestLogin:
     def teardown(self):
         time.sleep(2)
         self.driver.quit()
-
-    def test_login(self):
+    @pytest.mark.parametrize(("phone","password","expect"),analyze_file("login_data","test_login"))
+    def test_login(self,phone,password,expect):
         self.page.home_page.click_mine()
         self.page.mine_page.click_login_and_sign_up()
-        self.page.login_and_sign_up_page.input_phone("18513107283")
-        self.page.login_and_sign_up_page.input_password("123456")
+        self.page.login_and_sign_up_page.input_phone(phone)
+        self.page.login_and_sign_up_page.input_password(password)
         self.page.login_and_sign_up_page.click_login()
-        assert self.page.login_and_sign_up_page.find_toast("登录成功")
+        assert self.page.login_and_sign_up_page.find_toast(expect)
 
 
     def test_login_f(self):
