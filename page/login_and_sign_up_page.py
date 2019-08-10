a3769759488eed8cc5862056380c2254da0de1a9
lcysweet <lcy@163.com>
2019-08-10 18:26:37 +0800

    判断增加 显示密码按钮是否可用

--- page/login_and_sign_up_page.py
+++ page/login_and_sign_up_page.py
@@ -13,6 +13,9 @@ class LoginAndSignUpPage(BaseAction):
     # 登录
     login_button = By.ID, "com.tpshop.malls:id/login_tv"
 
+    # 显示密码按钮
+    show_password_button = By.ID, "com.tpshop.malls:id/view_pwd_img"
+
     # 输入 账号
     @allure.step(title="登录页面 - 输入 账号")
     def input_phone(self, text):
@@ -28,6 +31,10 @@ class LoginAndSignUpPage(BaseAction):
     def click_login(self):
         self.click(self.login_button)
 
+    # 点击 显示密码眼睛
+    @allure.step(title="登录页面 - 点击 显示密码眼睛")
+    def click_show_password_eys(self):
+        self.click(self.show_password_button)
 
     # 判断 登录后toast文本内容
     @allure.step(title='判断登录后toast文本')
@@ -48,7 +55,6 @@ class LoginAndSignUpPage(BaseAction):
         :return: 是否可用
         """
         return self.find_element(self.login_button).get_attribute(attribute) == "true"
-
         # if self.find_element(self.login_button).get_attribute("enabled") == "true":
         #     return True
         # else:
@@ -57,4 +63,17 @@ class LoginAndSignUpPage(BaseAction):
         #     self.find_get_attribute(self.login_button)
         #     return True
         # except Exception as e:
+        #     return False
+
+    def is_password_eys_show(self,password):
+        """
+       根据传入的密码，判断和密码框上的密码是否一致可见
+       :param password: 传入的密码
+       :return: 是否一致
+       """
+        return self.find_element(self.password_edit_text).text == password
+        # try:
+        #     self.find_element((By.XPATH, "//*[@text='" + password + "']"))
+        #     return True
+        # except:
         #     return False
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,4 +1,7 @@
+import random
 import time
+
+import allure
 import pytest
 import sys
 sys.path.append('D:\\Projects\workes\\user_login')
@@ -6,6 +9,11 @@ from base.base_driver import init_driver
 from page.page import Page
 from base.base_analyze import analyze_data
 
+def random_password():
+    password = ""
+    for i in range(8):
+        password += str(random.randint(0, 9))
+    return password
 
 class TestLogin:
 
@@ -16,27 +24,15 @@ class TestLogin:
     def teardown(self):
         time.sleep(2)
         self.driver.quit()
-    # @pytest.mark.parametrize(("phone","password","expect"),analyze_data("login_data","test_login"))
-    # def test_login(self,phone,password,expect):
-    #     # 首页 - 点击 我
-    #     self.page.home_page.click_mine()
-    #     # 注册 - 点击 登录/注册
-    #     self.page.mine_page.click_login_and_sign_up()
-    #     # 登录- 输入 用户名
-    #     self.page.login_and_sign_up_page.input_phone(phone)
-    #     # 登录- 输入 密码
-    #     self.page.login_and_sign_up_page.input_password(password)
-    #     # 登登录 - 点击 登录
-    #     self.page.login_and_sign_up_page.click_login()
-    #     # 断言 toast
-    #     assert self.page.login_and_sign_up_page.find_toast(expect)
 
+    # 1登录参数化正常流程
     @pytest.mark.parametrize("args", analyze_data("login_data", "test_login"))
     def test_login(self, args):
-        # 获取数据
+        # 解析数据
         username = args["phone"]
         password = args["password"]
         toast = args["expect"]
+
         # 首页 - 点击 我
         self.page.home_page.click_mine()
         # 注册 - 点击 登录/注册
@@ -49,3 +45,53 @@ class TestLogin:
         self.page.login_and_sign_up_page.click_login()
         # 断言 toast
         assert self.page.login_and_sign_up_page.find_toast(toast)
+
+     # 2判断 登录名和密码有一个为空登录按钮是否可用状态
+    @pytest.mark.parametrize("args", analyze_data("login_data_if", "test_login_if"))
+    def test_login_if(self, args):
+        # 获取数据
+        username = args["phone"]
+        password = args["password"]
+        # 首页 - 点击 我
+        self.page.home_page.click_mine()
+        # 注册 - 点击 登录/注册
+        self.page.mine_page.click_login_and_sign_up()
+        # 登录- 输入 用户名
+        self.page.login_and_sign_up_page.input_phone(username)
+        # 登录- 输入 密码
+        self.page.login_and_sign_up_page.input_password(password)
+        # 判断登录按钮是否可用
+        assert not self.page.login_and_sign_up_page.is_login_enabled("enabled")
+        # if self.page.login_and_sign_up_page.is_login_enabled() == False:
+        #     assert True
+        # else:
+        #     assert False
+        # 登录名和密码有一个为空判断登录按钮是否可用状态
+
+    # 3判断 密码眼睛是否可用
+    @pytest.mark.parametrize("password", [random_password(), random_password()])
+    def test_show_password(self,password):
+        # 首页 - 点击 我
+        self.page.home_page.click_mine()
+        # 注册 - 点击 登录/注册
+        self.page.mine_page.click_login_and_sign_up()
+        # 登录- 输入 密码
+        self.page.login_and_sign_up_page.input_password(password)
+        # 判断 输入的密码找不到
+        assert not self.page.login_and_sign_up_page.is_password_eys_show(password)
+        # 点击显示密码
+        self.page.login_and_sign_up_page.click_show_password_eys()
+        # 找到 输入的密码
+        assert self.page.login_and_sign_up_page.is_password_eys_show(password)
+
+        # # 判断-显示密码按钮 是否可用
+        # if not self.page.login_and_sign_up_page.is_password_eys_show(password):
+        #     # 点击显示密码
+        #     self.page.login_and_sign_up_page.click_show_password_eys()
+        #     # 截图，上传到报告
+        #     allure.attach("截图：", self.driver.get_screenshot_as_png(), allure.attach_type.PNG)
+        #     # 找到显示密码 判断 找到的密码和显示的密码是否一致
+        #     assert self.page.login_and_sign_up_page.is_password_eys_show(password)
+
+
+
