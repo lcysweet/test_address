b668d9bfe5f57bb50826b55c14a67a5c4e3e82db
lcysweet <lcy@163.com>
2019-08-12 14:12:14 +0800

    优化 解决无序字典

--- base/base_analyze.py
+++ base/base_analyze.py
@@ -1,4 +1,5 @@
 # import yaml
+# 原始没有优化的方法
 # def analyze_data(file_name, key_name):
 #     # contact_data
 #     file_path = "./data/" + file_name + ".yaml"
@@ -10,6 +11,17 @@
 #             data_list.append(list(i.values()))
 #         return data_list
 
+# 解决字典无序列表问题--优先使用
+# def analyze_data(file_name, key_name):
+#     # contact_data
+#     file_path = "./data/" + file_name + ".yaml"
+#     with open(file_path, "r", encoding='utf-8') as f:
+#
+#         data = yaml.load(f)
+#         data_list = list()
+#         for i in data[key_name].values():
+#             data_list.append(i)
+#         return data_list
 
 import os
 import yaml
@@ -24,3 +36,6 @@ def analyze_data(file_name, key):
         data_list = list()
         data_list.extend(yaml.load(f)[key].values())
         return data_list
+
+# 字典 -
+# [[],[],[]]
